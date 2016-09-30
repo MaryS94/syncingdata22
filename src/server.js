adf8057e972750f10d6afa930779d39b6d7c5bdb
MaryS94 <MaryS94@users.noreply.github.com>
2016-09-30 07:11:36 -0400

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/index.html`);
+
+const onRequest = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const app = http.createServer(onRequest).listen(port);
+console.log(`Listening on 127.0.0.1: ${port}`);
+
+const io = socketio(app);
+
+
+// draw rectangle
+const onDraw = (sock) => {
+  const socket = sock;
+
+  socket.on('draw', (data) => {
+    socket.broadcast.emit('draw',
+      {
+        x: data.x,
+        y: data.y,
+        height: data.height,
+        width: data.width,
+        imgData: data.imgData });
+  });
+};
+
+
+io.sockets.on('connection', (socket) => {
+  onDraw(socket);
+});
+
+console.log('Websocket server started');
